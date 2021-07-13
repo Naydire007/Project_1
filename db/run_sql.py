a85fa26a3989cacbf63c43855eac013105acf80c
Stephanie <stephania.l.e@gmail.com>
2021-07-13 12:13:12 +0100

    sql fixed

--- db/run_sql.py
+++ db/run_sql.py
@@ -4,3 +4,17 @@ import psycopg2.extras as ext
 def run_sql (sql, values = None):
     conn = None
     results = []
+
+    try:
+        conn=psycopg2.connect("dbname='taks_manager'")
+        cur= conn.cursor(cursor_factory=ext.DictCursor)
+        cur.exectue(sql,values)
+        conn.commit()
+        results=cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+        return results
