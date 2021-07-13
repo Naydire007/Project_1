10347410bb729e5bd5c5becbf97f9bcf66b916eb
Stephanie <stephania.l.e@gmail.com>
2021-07-13 02:52:20 +0100

    added some users

---
+++ console.py
@@ -0,0 +1,23 @@
+import pdb
+from models.inventory import Inventory
+from models.manufacturers import Manufacturers
+
+import repositories.manufacturer_repository as manufacturer_repository
+import repositories.inventory_repository as inventory_repository
+
+inventory_repository.delete()
+manufacturer_repository.delete()
+
+
+manufacturer1 = Manufacturers("Magic Life","Useful product for your quests")
+manufacturer_repository.save(manufacturer1)
+manufacturer2 = Manufacturers("Potion Life", "Saving you again")
+manufacturer_repository.save(manufacturer2)
+manufacturer_repository.select_all()
+
+item1 = Inventory("Healing potion", manufacturer1, "Heals superficial wounds", 3, 20,30)
+inventory_repository.save(item1)
+item2 = Inventory("Resurrect", manufacturer2, "Heals superficial wounds", 1, 25,50)
+inventory_repository.save(item2)
+
+pdb.set_trace()
